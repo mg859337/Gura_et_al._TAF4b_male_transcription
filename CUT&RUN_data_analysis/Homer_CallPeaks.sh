3b930136e394e6348c40cb7de3c728914b72f7d5
mg859337 <48734633+mg859337@users.noreply.github.com>
2021-11-23 14:25:16 -0500

    Update Homer_CallPeaks.sh

--- CUT&RUN_data_analysis/Homer_CallPeaks.sh
+++ CUT&RUN_data_analysis/Homer_CallPeaks.sh
@@ -17,9 +17,8 @@ module load samtools
 
 # call peaks
 
+findPeaks ../M-TAF4b_1_1.120/ -style factor -P 0.0015 -fdr 0.01 -F 2.0 -L 2 -LP 0.0015 -i ../M-IgG_1_1.120/ -o ./M-TAF4b_1_peaks.txt
+findPeaks ../M-TAF4b_2_1.120/ -style factor -P 0.0015 -fdr 0.01 -F 2.0 -L 2 -LP 0.0015 -i ../M-IgG_2_1.120/ -o ./M-TAF4b_2_peaks.txt
 
-findPeaks ../F-TAF4b_1_1.120/ -style factor -P 0.0015 -fdr 0.01 -F 2.0 -L 2 -LP 0.0015 -i ../F-IgG_1_1.120/ -o ./F-TAF4b_1_peaks.txt
-findPeaks ../F-TAF4b_2_1.120/ -style factor -P 0.0015 -fdr 0.01 -F 2.0 -L 2 -LP 0.0015 -i ../F-IgG_2_1.120/ -o ./F-TAF4b_2_peaks.txt
-
-findPeaks ../F-H3K4_1_150.500/ -style histone -P 0.0015 -fdr 0.01 -F 2.0 -L 2 -LP 0.0015 -i ../F-IgG_1_150.500/ -o ./F-H3K4_1_peaks.txt
-findPeaks ../F-H3K4_2_150.500/ -style histone -P 0.0015 -fdr 0.01 -F 2.0 -L 2 -LP 0.0015 -i ../F-IgG_2_150.500/ -o ./F-H3K4_2_peaks.txt
+findPeaks ../M-H3K4_1_150.500/ -style histone -P 0.0015 -fdr 0.01 -F 2.0 -L 2 -LP 0.0015 -i ../M-IgG_1_150.500/ -o ./M-H3K4_1_peaks.txt
+findPeaks ../M-H3K4_2_150.500/ -style histone -P 0.0015 -fdr 0.01 -F 2.0 -L 2 -LP 0.0015 -i ../M-IgG_2_150.500/ -o ./M-H3K4_2_peaks.txt
