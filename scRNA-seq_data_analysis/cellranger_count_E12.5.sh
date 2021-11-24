28fe10042a2fd6029eb016ec35326d4b944c87c6
mg859337 <48734633+mg859337@users.noreply.github.com>
2021-11-24 11:49:11 -0500

    Update cellranger_count_E12.5.sh

--- scRNA-seq_data_analysis/cellranger_count_E12.5.sh
+++ scRNA-seq_data_analysis/cellranger_count_E12.5.sh
@@ -3,25 +3,20 @@
 # Request resources: 
 #SBATCH --time=40:00:00 
 #SBATCH --cpus-per-task=32
-#SBATCH --mem=120G
+#SBATCH --mem=246G
 
 # Specify a job name: 
 #SBATCH -J cellranger_count
 
 # Specify output and error files: 
-#SBATCH -o outputfilecount_E12.out
-#SBATCH -e errorfilecount_E12.err
+#SBATCH -o outputfilecount.out
+#SBATCH -e errorfilecount.err
 
-module load python/3.5.2
-cd ..
-source cellranger.venv/bin/activate
+module load cellranger/6.0.0 
 
-cd cellranger.venv/
-cd cellranger-5.0.0/
-
-export PATH=/users/mgura/scratch/cellranger.venv/cellranger-5.0.0:$PATH
+# Change id, fastqs, and sample based on sample name and file locations. E12.5 used as an example
 
 cellranger count 	--id=E12_germ_cells \
 					--transcriptome=../refdata-gex-mm10-2020-A \
 					--fastqs=/users/mgura/scratch/Zhao \
-					--sample=E12
+					--sample=E12.5
