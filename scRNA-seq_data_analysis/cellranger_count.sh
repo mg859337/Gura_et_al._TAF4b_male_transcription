71af860b9fe6e191180fde651e226a1732f36ea9
mg859337 <48734633+mg859337@users.noreply.github.com>
2021-11-24 11:56:03 -0500

    Update cellranger_count.sh

--- scRNA-seq_data_analysis/cellranger_count.sh
+++ scRNA-seq_data_analysis/cellranger_count.sh
@@ -17,6 +17,6 @@ module load cellranger/6.0.0
 # Change id, fastqs, and sample based on sample name and file locations. E12.5 used as an example
 
 cellranger count 	--id=E12.5 \
-					--transcriptome=../refdata-gex-mm10-2020-A \
-					--fastqs=/users/mgura/scratch/Zhao \
-					--sample=Nguyen_E12
+			--transcriptome=../refdata-gex-mm10-2020-A \
+			--fastqs=/users/mgura/scratch/Nguyen_E12_E13/Nguyen_E12 \
+			--sample=Nguyen_E12
